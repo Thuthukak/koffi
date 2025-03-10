97faec1614b7e6b42e01a5fd65141494f4394e46
Wisdom Banda <wisdombanda57@gmail.com>
2025-03-10 19:36:56 +0200

    Created models and notification classes:  - Added a reminders class  - Added a bookingConfirmation class  - Added client fields in the database

---
+++ database/migrations/2025_03_10_165553_create_clients_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('clients', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('phoneNumber')->nullable()->unique();
+            $table->string('status')->default('queued'); // in queue, skipped or done cutting
+            $table->string('skipCount')->default(0); // number of times skipped
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamp('bookingSlot');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('clients');
+    }
+};
